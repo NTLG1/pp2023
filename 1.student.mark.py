345b2afdea039f28a69b80abb40f43d3ba83819a
NTLG1 <longntl1009@gmail.com>
2023-03-20 21:03:33 +0700

    Add comments and minor changes

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,45 +1,59 @@
 studentlist=[]
 courselist={}
+
+#Function to enter students' information
 def input_students():
     n=int(input(("Enter the number of students: ")))
     for i in range(0,n,1):
+        #Put all inputted students' information into studentlist list
         studentlist.append({"id":input("Enter student's id: "), "name":input("Enter the student's name: "), "DoB":input("Enter student's DoB: "),"courseid": [], "mark": []})
+
+#Function to enter courses' information
 def input_courses():       
     coursenum= int(input("Enter the number of course: "))
     for i in range (0,coursenum,1):
         id =input("Enter course id: ")
         name=input("Enter course name: ")
+        #Put all inputted courses' information into courselist dict with index is the id of the course
         courselist[id]={"id":id,"name":name}
 
+#Function to enter students' mark
 def input_marks():
     course_id = input("Enter the course ID: ")
     try:
+        #check if the inputted course's id exists in courselist
         courselist[course_id]
     except:
         print("Invalid course ID")
         return
     for i in range(0,len(studentlist),1):
+        #put mark and course's id information into studentlist
         studentlist[i]["courseid"].append(course_id)
-        mark= int(input("Enter the mark for "+studentlist[i].get("id")+" "+studentlist[i].get("name")+": "))
+        mark= float(input("Enter the mark for "+studentlist[i].get("id")+" "+studentlist[i].get("name")+": "))
         studentlist[i]["mark"].append(mark)
 
+#Function to display courses
 def show_courses():
     for element in (courselist):
         print(courselist[element].get("id")," ",courselist[element].get("name"))
 
+#Function to display students
 def show_students():
     for i in range(0,len(studentlist),1):
         print(studentlist[i].get("id")," ",studentlist[i].get("name")," DoB:",studentlist[i].get("DoB"))
 
+#Function to display marks
 def show_marks():
     course_id = input("Enter the course ID: ")
     try:
+        #check if the inputted course's id exists in courselist
         courselist[course_id]
     except:
         print("Invalid course ID")
         return
     for i in range(0,len(studentlist),1):   
         try:
+            #Search if student's mark exists and if it does then x is the index of mark list to point at
             x= studentlist[i]["courseid"].index(course_id)
             print(studentlist[i].get("id")," ",studentlist[i].get("name")," ",courselist[course_id].get("name"),"mark: ",studentlist[i].get("mark")[x])
         except:
@@ -47,6 +61,7 @@ def show_marks():
 
 input_students()
 input_courses()
+#Menu
 while True:
     print("Select an option")
     print("1. Input marks for a course")
--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -1,6 +1,7 @@
 studentlist=[]
 courselist={}
 
+#Create a class Student that contains student's information
 class Student():
     def __init__(self):
         self.__id = input("Enter student's id: ")
@@ -30,6 +31,7 @@ class Student():
     def set_mark(self, mark):
         self.__mark.append(mark)
 
+#Create a class Course that contains course's information
 class Course():
     def __init__(self,id, name):
         self.__id = id
@@ -41,54 +43,67 @@ class Course():
     def get_name(self):
         return self.__name
 
+#Function to enter students' information
 def input_students():
     n = int(input("Enter number of student: "))
     for i in range(0,n,1):
+        #Put all inputted students' information into studentlist list
         studentlist.append(Student())
 
+#Function to enter courses' information
 def input_courses():       
     coursenum= int(input("Enter the number of course: "))
     for i in range (0,coursenum,1):
         id = input("Enter course id: ")
         name = input("Enter course name: ")
+        #Put all inputted courses' information into courselist dict with index is the id of the course
         courselist[id] = Course(id,name)
 
+#Function to enter students' mark
 def input_marks():
     course_id = input("Enter the course ID: ")
     try:
+        #check if the inputted course's id exists in courselist
         courselist[course_id]
     except:
         print("Invalid course ID")
         return
     for i in range(0,len(studentlist),1):
+        #put mark and course's id information into studentlist
         studentlist[i].set_courseid(course_id)
-        mark= int(input("Enter the mark for "+studentlist[i].get_id()+" "+studentlist[i].get_name()+": "))
+        mark= float(input("Enter the mark for "+studentlist[i].get_id()+" "+studentlist[i].get_name()+": "))
         studentlist[i].set_mark(mark)
 
+#Function to display courses
 def show_courses():
     for element in (courselist):
         print(courselist[element].get_id()," ",courselist[element].get_name())
 
+#Function to display students
 def show_students():
     for Student in studentlist:
         print(f"{Student.get_id()} {Student.get_name()} DoB: {Student.get_DoB()}")
 
+#Function to display marks
 def show_marks():
     course_id = input("Enter the course ID: ")
     try:
+        #check if the inputted course's id exists in courselist
         courselist[course_id]
     except:
         print("Invalid course ID")
         return
     for i in range(0,len(studentlist),1):   
         try:
+            #Search if student's mark exists and if it does then x is the index of mark list to point at
             x= studentlist[i].get_courseid().index(course_id)
-            print(course_id," ",studentlist[i].get_name()," ",courselist[course_id].get_name(),"mark: ",studentlist[i].get_mark()[x])
+            print(studentlist[i].get_id()," ",studentlist[i].get_name()," ",courselist[course_id].get_name(),"mark: ",studentlist[i].get_mark()[x])
         except:
-            print(course_id," ",studentlist[i].get_name()," ",courselist[course_id].get_name(),"mark: N/A")
+            print(studentlist[i].get_id()," ",studentlist[i].get_name()," ",courselist[course_id].get_name(),"mark: N/A")
 
 input_students()
 input_courses()
+#Menu
 while True:
     print("Select an option")
     print("1. Input marks for a course")
